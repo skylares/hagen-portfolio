8274480ac15f3c95e5c626311ddd54a93dde6d3b
Skylar Kesselring <skylarkesselring@gmail.com>
2022-07-09 23:42:16 -0400

    Temp change

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,54 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Home from './components/Home';
-import Piano from './components/Piano';
-import Lionfisher from './components/Lionfisher';
-import Professional from './components/Professional';
-import Skeetshooter from './components/Skeetshooter';
-import Startup from './components/Startup';
 import resume from './documents/hagen_oneill_resume.pdf';
 
 export default function App() {
   return (
-    <div>  
-    <Router>
-      <div className="app">
-      <nav className="app--nav">
-        <ul className="app--nav-ul"> 
-          <li className="app--nav-li -home"><Link to="/">Hagen O'Neill</Link></li>
-          <li className="app--nav-li"><Link to="/projects/lionfish-hunter">Lionfish Hunter</Link></li>
-          <li className="app--nav-li"><Link to="/projects/piano-hero">Piano Hero</Link></li>
-          <li className="app--nav-li"><Link to="/projects/automated-skeet-shooter">Automated Skeet Shooter</Link></li>
-          <li className="app--nav-li"><Link to="/experience/start-up">Start-up</Link></li>
-          <li className="app--nav-li"><Link to="/experience/professional">Professional Experience</Link></li>
-          <li className="app--nav-li"><a href={resume}>Resume</a></li>
-        </ul>
-      </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects/lionfish-hunter" element={<Lionfisher />} />
-          <Route path="/projects/piano-hero" element={<Piano />} />
-          <Route path="/projects/automated-skeet-shooter" element={<Skeetshooter />} />
-          <Route path="/experience/start-up" element={<Startup />} />
-          <Route path="/experience/professional" element={<Professional />} />
-        </Routes>
-      </div>
-    </Router>
-    </div>
+    <embed className="temp--resume" src={resume}/>
   );
 }
+
+
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// import Home from './components/Home';
+// import Piano from './components/Piano';
+// import Lionfisher from './components/Lionfisher';
+// import Professional from './components/Professional';
+// import Skeetshooter from './components/Skeetshooter';
+// import Startup from './components/Startup';
+// import resume from './documents/hagen_oneill_resume.pdf';
+
+// export default function App() {
+//   return (
+//     <div>  
+//     <Router>
+//       <div className="app">
+//       <nav className="app--nav">
+//         <ul className="app--nav-ul"> 
+//           <li className="app--nav-li -home"><Link to="/">Hagen O'Neill</Link></li>
+//           <li className="app--nav-li"><Link to="/projects/lionfish-hunter">Lionfish Hunter</Link></li>
+//           <li className="app--nav-li"><Link to="/projects/piano-hero">Piano Hero</Link></li>
+//           <li className="app--nav-li"><Link to="/projects/automated-skeet-shooter">Automated Skeet Shooter</Link></li>
+//           <li className="app--nav-li"><Link to="/experience/start-up">Start-up</Link></li>
+//           <li className="app--nav-li"><Link to="/experience/professional">Professional Experience</Link></li>
+//           <li className="app--nav-li"><a href={resume}>Resume</a></li>
+//         </ul>
+//       </nav>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/projects/lionfish-hunter" element={<Lionfisher />} />
+//           <Route path="/projects/piano-hero" element={<Piano />} />
+//           <Route path="/projects/automated-skeet-shooter" element={<Skeetshooter />} />
+//           <Route path="/experience/start-up" element={<Startup />} />
+//           <Route path="/experience/professional" element={<Professional />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//     </div>
+//   );
+// }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import './styles.css';
 
+//! remove
+import resume from './documents/hagen_oneill_resume.pdf';
+//!
+
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error('Failed to find root element');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    //! fix
+     <embed className="temp--resume" src={resume}/>
+    {/* <App /> */}
+    //!
   </StrictMode>
 );
